bc2dbfcf47bb19ae35467fadb70710f33ab57bcb
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-28 00:43:55 +0300

    Add various data types

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int myInt =120;
+        long myLong = 28292093883920220L;
+        byte myByte = 100;
+        float myFloat = 223.14f;
+        double myDouble = 23.123;
+
+        System.out.println("My number is: "+myInt);
+        System.out.println(myLong);
+        System.out.println(myByte);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+
+
         }
     }
-}
